45e47b7944ea266bd9a364c446bed6b7fcde8708
Nikita Borisov <nb@zvooq.com>
2017-11-30 17:38:46 +0300

    Fix warning.

--- Realm+JSON/RLMObject+JSON.h
+++ Realm+JSON/RLMObject+JSON.h
@@ -23,7 +23,7 @@
 - (id)primaryKeyValue;
 + (id)primaryKeyValueFromJSONDictionary:(NSDictionary *)dictionary;
 
-- (void)performInTransaction:(void (^)())transaction;
+- (void)performInTransaction:(void (^)(void))transaction;
 - (void)removeFromRealm;
 
 @end
